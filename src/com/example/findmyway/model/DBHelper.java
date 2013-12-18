3815c6dc7658e7d0ea45b2f64c615a6710bc06c5
liuweilong <viclong1993@gmail.com>
2013-12-18 20:29:48 +0800

    Add a professor's name

--- src/com/example/findmyway/model/DBHelper.java
+++ src/com/example/findmyway/model/DBHelper.java
@@ -75,7 +75,7 @@ public class DBHelper extends SQLiteOpenHelper  {
     	insertRoom("Office 3-13", "Jason Gu", 3,13,2,5,432,480);
     	insertRoom("Office 3-14", "Dawn Koh Chin-Ing", 3,14,2,5,344,480);
     	insertRoom("Office 3-15", "Casey Hammond", 3,15,2,5,267.6,480);
-    	insertRoom("Office 3-16", "", 3,16,2,5,344,744);
+    	insertRoom("Office 3-16", "Nagi-Man Cheung", 3,16,2,5,344,744);
     	insertRoom("Office 3-17", "Lui Siu Hang Simon", 3,17,2,5,432,744);
     	insertRoom("Office 3-18", "", 3,18,2,4,80,744);
     	insertRoom("Office 3-19", "James Rowlins", 3,19,2,4,160,744);
